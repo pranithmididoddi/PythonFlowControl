c8a3095ddca99c666fd4e4a9089358116d4cda3d
pranith <psmididoddi@gmail.com>
2016-12-13 17:52:07 -0600

    commit2: Includes flow control using if, else, elif

--- pythonFloControl.py
+++ pythonFloControl.py
@@ -1,1 +1,19 @@
-print("Hi")
+print("Enter the number between 1 and 10")
+number=int(input())
+
+if number<5:
+    print("guess a larger number: ")
+    number=int(input())
+    if number==5:
+        print("You guessed it correct")
+    else:
+        print("yOu guessed it wrong")
+elif number>5:
+    print("guess a smaller number")
+    number = int(input())
+    if number==5:
+        print("You guessed it correct")
+    else:
+        print("yOu guessed it wrong")
+else:
+    print("You guessed it right at the first guess")
